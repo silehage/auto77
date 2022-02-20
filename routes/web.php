ce43d1e8acfa6d233d7436c970d146c55f88128e
silehage <silehwage@gmail.com>
2022-02-21 03:18:52 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -19,9 +19,6 @@ use App\Http\Resources\ProductCollection;
 |
 */
 
-Route::get('test', [ProductController::class, 'index']);
-Route::get('show/{id}', [ProductController::class, 'show']);
-
 Route::get('/', [FrontController::class, 'homepage']);
 Route::get('products', [FrontController::class, 'products']);
 Route::get('products/category/{category}', [FrontController::class, 'productCategory']);
@@ -30,49 +27,3 @@ Route::get('posts', [FrontController::class, 'postIndex']);
 Route::get('post/{slug}', [FrontController::class, 'postDetail']);
 Route::get('clear-cache', [FrontController::class, 'clearCache']);
 Route::get('/{any}', [FrontController::class, 'any'])->where('any','^(?!api).*$');
-
-function set($data)
-{
-  $pricing = [
-    'default_price' => $data->price,
-    'current_price' => $data->price,
-    'discount_value' => 0,
-    'discount_percent' => 0,
-    'is_discount' => false 
-];
-
-$disc = null;
-
-if($data->discount_id) {
-    $disc = $data->discount;
-} elseif($data->promote_id) {
-    $disc = $data->promote->discount;
-}
-
-if($disc) {
-
-    $pricing['is_discount'] = true;
-
-    $discValue = 0;
-    
-
-    if($disc->unit == 'percent') {
-
-        $discValue = ($data->price*$disc->value) / 100;
-
-        $pricing['current_price'] = $data->price - ($data->price*$disc->value / 100);
-        $pricing['discount_percent'] = $disc->value;
-        
-     } else{
-
-         $discValue = $disc->value;
-         $pricing['current_price'] = $data->price - (int) $disc->value;
-         $pricing['discount_percent'] = number_format(((int)$disc->value / $data->price)*100, 1);
-
-    }
-
-    $pricing['discount_value'] = $discValue;
- }
-
-return $pricing;
-}
