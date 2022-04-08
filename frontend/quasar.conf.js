6901266ed0b06e8a854eb4e688c9d0d41bdcfbe0
silehage <silehwage@gmail.com>
2022-04-08 22:02:44 +0700

    test theme

--- frontend/quasar.conf.js
+++ frontend/quasar.conf.js
@@ -24,7 +24,7 @@ module.exports = function (ctx ) {
     // https://v1.quasar.dev/quasar-cli/boot-files
     boot: [
       
-      'axios'
+      'axios','helpers'
     ],
 
     // https://v1.quasar.dev/quasar-cli/quasar-conf-js#Property%3A-css
