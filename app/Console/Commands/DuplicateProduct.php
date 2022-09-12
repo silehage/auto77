f04d08d1ce050a05fd7fc451f20fe69833445588
silehage <silehwage@gmail.com>
2022-09-13 03:05:40 +0700

    change duplicate product

--- app/Console/Commands/DuplicateProduct.php
+++ app/Console/Commands/DuplicateProduct.php
@@ -2,11 +2,13 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use App\Models\Asset;
 use App\Models\Product;
 use App\Models\Category;
 use Illuminate\Support\Str;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 
 class DuplicateProduct extends Command
 {
@@ -47,26 +49,36 @@ class DuplicateProduct extends Command
 
         $categories = Category::all();
 
-        foreach($categories as $category) {
+        DB::beginTransaction();
 
-            foreach($products as $product) {
+        try {
 
-                $assets = $product->assets;
-                $productArr = $product->toArray();
-                
-                $length ++;
-                $title = 'Produk #' . $length;
-                
-                $productArr['title'] = 'Produk #' . $length;
-                $productArr['slug'] = Str::slug($title);
-                $productArr['category_id'] = $category->id;
-    
-                $p = Product::create($productArr);
-    
-                $p->assets()->create([
-                    'filename' => $assets[0]->filename
-                ]);
+            foreach($categories as $category) {
+
+                foreach($products as $product) {
+
+                    $assets = $product->assets;
+                    $productArr = $product->toArray();
+                    
+                    $length ++;
+                    $title = 'Produk #' . $length;
+                    
+                    $productArr['title'] = 'Produk #' . $length;
+                    $productArr['slug'] = Str::slug($title);
+                    $productArr['category_id'] = $category->id;
+        
+                    $p = Product::create($productArr);
+        
+                    $p->assets()->create([
+                        'filename' => $assets[0]->filename
+                    ]);
+                }
             }
+            DB::commit();
+
+        } catch (Exception $e) {
+            
+            DB::rollback();
         }
 
     }
